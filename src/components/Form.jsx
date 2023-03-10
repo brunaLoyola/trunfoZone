6587beb267158b589b54f282e8f524b2e092b67b
Bruna Loyola <brunloyola02@gmail.com>
2023-03-09 22:37:32 -0300

    Teste

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -113,6 +112,7 @@ class Form extends React.Component {
             data-testid="trunfo-input"
             id="trunfo-input"
             checked={ cardTrunfo }
+            { ...hasTrunfo }
             onChange={ onInputChange }
             name="cardTrunfo"
           />
